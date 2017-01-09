5bdaf4c40b1280a47d72478d4d2199707689a7d8
divya <divya.maharshi@igp.com>
2017-01-09 14:16:22 +0530

    final similarity code  for cards and categories

--- product_similarity_categorywise.R
+++ product_similarity_categorywise.R
@@ -43,7 +43,7 @@ products_cosine_measure <- function(set_type,distinct_cardid)
       
       ###rrop card_id computre similarity based on  all sets
       if (set_type == TRUE){
-        sql_query = paste("select t1.products_id,t2.label,t1.weightage from newigp_products_to_attr_val t1, newigp_master_attr_vals t2, newigp_master_attr_sets t3 where t1.attr_val_id = t2.id and t2.attr_set_id =t3.id and t1.products_id in (",paste(cardid_products,collapse = ','),")",sep='')
+        sql_query = paste("select t1.products_id,t2.label,t1.weightage from newigp_products_to_attr_val t1, newigp_master_attr_vals t2, newigp_master_attr_sets t3 where t1.attr_val_id = t2.id and t2.attr_set_id =t3.id and t3.attr_set_type = 1 and  t1.products_id in (",paste(cardid_products,collapse = ','),")",sep='')
         sql_query <-  gsub("c\\(","\\(" , sql_query)
         rrop_data <- dbGetQuery(igpnewConnProd,sql_query)
        
@@ -80,12 +80,12 @@ products_cosine_measure <- function(set_type,distinct_cardid)
         actual_product_ids <- as.data.frame(rrop_data$products_id)
         actual_product_ids$seq_numbers <- seq(1:nrow(rrop_data))
         
+    
         # remove products_id for simil function
         rrop_data <- within(rrop_data,rm(products_id))
         
         # simil calculation 
-        rrop_mat1 <- rrop_data
-        cosine_simil <-simil(rrop_mat1,method = 'cosine',upper = TRUE, diag = TRUE)
+        cosine_simil <-simil(rrop_data,method = 'cosine',upper = TRUE, diag = TRUE)
         cosine_df <- melt(as.matrix(cosine_simil), varnames=c("row","col"))
         names(cosine_df) <- c('product1','product2','cosine_simil')
         
@@ -97,7 +97,6 @@ products_cosine_measure <- function(set_type,distinct_cardid)
         final_df_with_productids$cardid <- cardid
         temp_list[[i]] <- final_df_with_productids
         
-        print('done')
       }
     }
   }
@@ -105,40 +104,39 @@ products_cosine_measure <- function(set_type,distinct_cardid)
 }
 
 
-### RROP cards
+############################################## RROP cards #######################################################
 distinct_cardid <- dbGetQuery(igpnewConnProd, "select distinct(card_id) as cardid from cards_url where type != Category and card_id = 121" ) 
 rrop_similarity_df = do.call(rbind, products_cosine_measure(set_type = TRUE,distinct_cardid))
 
-####################################### Calculation ########################################3 
-### arrange
+### Calculation
+### cosine arrange
 top_rrop_similarity_df<-rrop_similarity_df %>%
   group_by(product1,cardid) %>%
   arrange(desc(cosine_simil))
-
-# rbind and arrange  per product 20 similar products 
 top_simil_df <- top_rrop_similarity_df
 
-# remove duplicates and keep row with high cosine simil
-top_simil_df <- top_simil_df[!duplicated(top_simil_df[,c('product1','product2')]),]
+### for perticular product check 
+top_simil_df <- top_simil_df[top_simil_df['product1'] ==  217285, ]
 
+### select cosine based 100 products
 top_simil_df <- top_simil_df %>%
   group_by(product1,cardid) %>%
   arrange(desc(cosine_simil))  %>%
   slice(1:100)
 
-####for hetrogeneous merge with prod rank ####
+###for hetrogeneous merge with prod rank 
 ### products 1 price
 products1_price <- dbGetQuery(igpnewConnProd ,"select prod_id,p.products_mrp as product1_price  from products p join prod_rank pr  on p.products_id = pr.prod_id join product_cat pc on p.products_id = pc.pid join newigp_category_extra_info ci on pc.ptid = ci.categories_id join newigp_product_extra_info ei on pr.prod_id = ei.products_id where ci.cat_type =1  and  card_id =121 order by rank")
 products1_price <- unique(products1_price)
-top_simil_df<- merge(top_simil_df,products1_price,by.x = 'product1', by.y='prod_id')
+top_simil_df    <- merge(top_simil_df,products1_price,by.x = 'product1', by.y='prod_id')
 
 ###product2 price
 products <- dbGetQuery(igpnewConnProd, "select prod_id,rank,ptid,ei.flag_hamper,p.products_name_for_url,p.products_mrp as product2_price  from products p join prod_rank pr  on p.products_id = pr.prod_id join product_cat pc on p.products_id = pc.pid join newigp_category_extra_info ci on pc.ptid = ci.categories_id join newigp_product_extra_info ei on pr.prod_id = ei.products_id where ci.cat_type =1  and  card_id =121 order by rank")
-## hampers tagged in multiple ptid ----> do unique 
+###hampers tagged in multiple ptid ----> do unique 
 products <- products[!duplicated(products$prod_id,products$ptid) ,]
 
 
-# merge products which are having price greater then or equal to product 1
+### price logic on top of cosine 
 top_simil_df = top_simil_df %>%
   inner_join(products, by = c("product2" = "prod_id")) %>%
   filter(product2_price >= product1_price | product2_price-product1_price >= -(product1_price * .60) )
@@ -146,53 +144,62 @@ top_simil_df = top_simil_df %>%
 ###select hetrogeneous based on simil/rank        
 top_simil_df <- top_simil_df %>%
   group_by(product1,cardid) %>%
-  arrange((rank))  %>%
-  slice(1:100)
+  arrange(desc(cosine_simil))  %>%
+  slice(1:40)
 
 ######################### to just have count product per pt in strip of 16 #############################################################
-top_simil_df <- top_simil_df[top_simil_df['product1'] == 521298, ]
-top_simil_df  <- top_simil_df %>%
-  group_by(product1,cardid,ptid) %>%
-  arrange((rank))  %>%
-  slice(1:2)
+# top_simil_df  <- top_simil_df %>%
+#   group_by(product1,cardid,ptid) %>%
+#   arrange((rank))  %>%
+#   slice(1:2)
 
-#############################make strip of 16 products ###########################################3333
+#############################make strip of 16 products ###########################################
 top_simil_df  <- top_simil_df %>%
   group_by(product1,cardid) %>%
-  arrange(desc(cosine_simil))  %>%
-  slice(1:16) 
+  arrange(rank)  %>%
+  slice(1:16)
 
-rrop_df <- top_simil_df
-################end ##############################################################################
+rrop_final_df <- top_simil_df
+#################end ##############################################################################
 
 
 
-###### non-rrop 
-distinct_cardid <- dbGetQuery(igpnewConnProd, "select distinct(card_id) as cardid from cards_url where type != Category and card_id = 19870" )
+###### for categories 
+distinct_cardid <- dbGetQuery(igpnewConnProd, "select distinct(card_id) as cardid from cards_url where type != Category and card_id = 21406" )
 attributes_similarity_df = do.call(rbind,products_cosine_measure(set_type = FALSE,distinct_cardid))
 
-
-#################   Manipulation
+#################   calculations 
 top_attributes_similarity_df <- attributes_similarity_df %>%
   group_by(product1,cardid) %>%
   arrange(desc(cosine_simil)) 
-  
+
 # rbind and arrange  per product 20 similar products 
 top_simil_df <- top_attributes_similarity_df
+top_simil_df <- top_simil_df[top_simil_df['product1'] ==  522976, ]
+
+####### price calculation 
+products1_price <- dbGetQuery(igpnewConnProd ,"select prod_id,p.products_mrp as product1_price  from products p join prod_rank pr  on p.products_id = pr.prod_id join product_cat pc on p.products_id = pc.pid join newigp_category_extra_info ci on pc.ptid = ci.categories_id join newigp_product_extra_info ei on pr.prod_id = ei.products_id where ci.cat_type =1  and p.products_status = 1 and  card_id =21406 order by rank")
+products1_price <- unique(products1_price)
+top_simil_df    <- merge(top_simil_df,products1_price,by.x = 'product1', by.y='prod_id')
+ 
+###product2 price
+products <- dbGetQuery(igpnewConnProd, "select prod_id,rank,ptid,ei.flag_hamper,p.products_name_for_url,p.products_mrp as product2_price  from products p join prod_rank pr  on p.products_id = pr.prod_id join product_cat pc on p.products_id = pc.pid join newigp_category_extra_info ci on pc.ptid = ci.categories_id join newigp_product_extra_info ei on pr.prod_id = ei.products_id where ci.cat_type =1  and p.products_status = 1 and  card_id =21406 order by rank")
+## hampers tagged in multiple ptid ----> do unique 
+products <- products[!duplicated(products$prod_id,products$ptid) ,]
+ 
+#price logic on top of cosine 
+top_simil_df = top_simil_df %>%
+   inner_join(products, by = c("product2" = "prod_id")) %>% 
+  filter((product2_price >= product1_price & product2_price <= (product1_price + (product1_price * .40))) | product2_price-product1_price >= -(product1_price * .60) )
+
 
 # arrange per product 20 products in descending order of simil score
 top_simil_df <- top_simil_df %>%
   group_by(product1,cardid) %>%
-  arrange(desc(cosine_simil)) 
-  
-# remove duplicates and keep row with high cosine simil
-top_simil_df <- top_simil_df[!duplicated(top_simil_df[,c('product1','product2')]),]
-
-top_simil_df <- top_simil_df %>%
-  group_by(product1,cardid) %>%
-  arrange(desc(cosine_simil))  %>%
+  arrange(desc(cosine_simil)) %>%
   slice(1:16)
-
+  
+category_final_df <- top_simil_df
 ################end ##############################################################################
 
 
