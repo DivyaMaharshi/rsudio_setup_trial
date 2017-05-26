191f3cd2673d0b3a8242127f3f4b174f734ba7ea
divya <divya.maharshi@igp.com>
2017-05-26 11:50:34 +0530

    merge added twitter analysis for NLP

--- ml_algos_listendata/logistic_regression.R
+++ ml_algos_listendata/logistic_regression.R
@@ -17,39 +17,41 @@ val<-mydata[-dt,]
 
 #Run Logistic Regression
 mylogistic <- glm(admit ~ ., data = train, family = "binomial")
-summary(mylogistic)$coefficient
+summary(mylogistic)$coefficient 
+
 
 #Stepwise Logistic Regression
 mylogit = step(mylogistic)
 
-#Logistic Regression Coefficient
-summary.coeff0 = summary(mylogit)$coefficient
-
-#Calculating Odd Ratios
-OddRatio = exp(coef(mylogit))
-summary.coeff = cbind(Variable = row.names(summary.coeff0), OddRatio, summary.coeff0)
-row.names(summary.coeff) = NULL
-
-
-
-#R Function : Standardized Coefficients
-stdz.coff <- function (regmodel) 
-{ b <- summary(regmodel)$coef[-1,1]
-sx <- sapply(regmodel$model[-1], sd)
-beta <-(3^(1/2))/pi * sx * b
-return(beta)
-}
-
-std.Coeff = data.frame(Standardized.Coeff = stdz.coff(mylogit))
-std.Coeff = cbind(Variable = row.names(std.Coeff), std.Coeff)
-row.names(std.Coeff) = NULL
-
-#Final Summary Report
-final = merge(summary.coeff, std.Coeff, by = "Variable", all.x = TRUE)
+# #Logistic Regression Coefficient
+# summary.coeff0 = summary(mylogit)$coefficient
+# 
+# #Calculating Odd Ratios
+# OddRatio = exp(coef(mylogit))
+# summary.coeff = cbind(Variable = row.names(summary.coeff0), OddRatio, summary.coeff0)
+# row.names(summary.coeff) = NULL
+# 
+# 
+# 
+# #R Function : Standardized Coefficients
+# stdz.coff <- function (regmodel) 
+# { b <- summary(regmodel)$coef[-1,1]
+# sx <- sapply(regmodel$model[-1], sd)
+# beta <-(3^(1/2))/pi * sx * b
+# return(beta)
+# }
+# 
+# std.Coeff = data.frame(Standardized.Coeff = stdz.coff(mylogit))
+# std.Coeff = cbind(Variable = row.names(std.Coeff), std.Coeff)
+# row.names(std.Coeff) = NULL
+# 
+# #Final Summary Report
+# final = merge(summary.coeff, std.Coeff, by = "Variable", all.x = TRUE)
 
 #Prediction
 pred = predict(mylogit,val[,-(1)], type = "response")
 finaldata = cbind(val, pred)
+
 
 #Storing Model Performance Scores
 library(ROCR)
