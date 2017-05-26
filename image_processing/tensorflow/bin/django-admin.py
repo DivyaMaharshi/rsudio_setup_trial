2dc2216155ba6e4ae154cdd5c27df4949a241579
divya <divya.maharshi@igp.com>
2017-05-26 19:19:08 +0530

    image processing

---
+++ image_processing/tensorflow/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/igp/rsudio_setup_trial/rsudio_setup_trial/image_processing/tensorflow/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
